df13372aa1f1d0c729bed87c846fe27967e6e175
-pee <pee@erkkila.org>
2017-12-04 08:06:37 +0000

    mist/eth wallet/ide

---
+++ ethereum-mist/runMist.sh
@@ -0,0 +1,33 @@
+#!/bin/bash
+#
+#
+# XAUTH bits from here: http://wiki.ros.org/docker/Tutorials/GUI
+#
+XSOCK=/tmp/.X11-unix
+XAUTH=/tmp/.docker.xauth
+# FIXME - might fail
+touch $XAUTH
+xauth nlist $DISPLAY | sed -e 's/^..../ffff/' | xauth -f $XAUTH nmerge -
+# END
+
+docker run --rm  -it \
+        --env="XAUTHORITY=${XAUTH}" \
+        --env="DISPLAY" \
+        --volume=$XSOCK:$XSOCK:rw \
+        --volume=$XAUTH:$XAUTH:rw \
+		--volume=/etc/localtime:/etc/localtime:ro \
+		--device /dev/snd \
+		--device /dev/dri \
+		--device /dev/video0 \
+		--group-add audio \
+		--group-add video \
+        --user $(id -u):$(id -g) \
+		--ipc host \
+		--name mist \
+		mist:latest "$@"
+
+
+
+# --volume="${HOME}/.slack:/slack/.config/Slack" \
+#
+#
