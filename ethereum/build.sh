1dfd43bc157f0e74c6804b1628d64cfad1bb2b62
P <pee@erkkila.org>
2017-12-22 10:21:35 +0000

    interim

--- ethereum/build.sh
+++ ethereum/build.sh
@@ -1,5 +1,17 @@
 #!/bin/bash
 #
-# build for main network
+# build things
 #
-docker-compose -f main-build.yml build
+docker-compose -f build.yml build \
+	--build-arg UID=1000 \
+	--build-arg GID=1000 \
+	eth-main
+
+docker-compose -f build.yml build \
+	--build-arg gen_nonce="0xe1de3db4be5ddead" \
+	--build-arg data_dir="/root/.ethereum" \
+	--build-arg chain_type="private" \
+	--build-arg run_bootnode="true" \
+	--build-arg gen_chain_id="1971" \
+	--build-arg bootnode_url="" \
+	eth-dev
